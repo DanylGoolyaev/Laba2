258f2a34e1f85b3b574ea1befa94e8508d42372e
DanylGoolyaev <84317323+DanylGoolyaev@users.noreply.github.com>
2021-09-26 21:33:07 +0300

    Задание 1

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Address
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+        public string Apartment { get; set; }
+        public string Index { get; set; }
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Address address = new Address();
+
+            address.Country = "Ukraine";
+            address.City = "Kiev";
+            address.Street = "Khreshchatyk Street";
+            address.House = "1";
+            address.Apartment = "1";
+            address.Index = "11111";
+
+            Console.WriteLine(address.Country);
+            Console.WriteLine(address.City);
+            Console.WriteLine(address.Street);
+            Console.WriteLine(address.House);
+            Console.WriteLine(address.Apartment);
+            Console.WriteLine(address.Index);
+
+            Console.ReadKey();
+        }
+    }
+}
