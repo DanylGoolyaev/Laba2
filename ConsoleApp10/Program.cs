f26ea35f42298d75e0feb8d1970b52cbeb81b32b
DanylGoolyaev <84317323+DanylGoolyaev@users.noreply.github.com>
2021-09-26 21:33:44 +0300

    Задание 2

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Rectangle
+    {
+        private double side1, side2;
+
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+        public Rectangle() { }
+
+        double AreaCalculator()
+        {
+            return side1 * side2;
+        }
+
+        double PerimeterCalculator()
+        {
+            return (side1 + side2) * 2;
+        }
+
+        public double Area { get { return this.AreaCalculator(); } }
+        public double Perimeter { get { return this.PerimeterCalculator(); } }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите сторону А прямоугольника -> ");
+            string x = Console.ReadLine();
+            double side1 = Int32.Parse(x);
+
+            Console.Write("Введите сторону Б прямоугольника -> ");
+            string y = Console.ReadLine();
+            double side2 = Int32.Parse(y);
+
+            Rectangle rectangle = new Rectangle(side1, side2);
+
+            Console.WriteLine("Площадь = {0}", rectangle.Area);
+            Console.WriteLine("Периметр = {0}", rectangle.Perimeter);
+
+        }
+    }
+}
