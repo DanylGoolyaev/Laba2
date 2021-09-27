3c19196fcef417b0cf4929129c333141fca0d775
DanylGoolyaev <84317323+DanylGoolyaev@users.noreply.github.com>
2021-09-27 22:37:18 +0300

    Задание 3

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    public class Book
+    {
+        public Title title { get; set; }
+        public Author author { get; set; }
+        public Content content { get; set; }
+
+    }
+    public class Title
+    {
+        public string name;
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write($"\nИмя книги: {name}. ");
+            Console.ResetColor();
+        }
+    }
+    public class Author
+    {
+        public string author;
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write($"\nАвтор книги: {author}. ");
+            Console.ResetColor();
+        }
+    }
+    public class Content
+    {
+        public string content;
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write($"\nКонтент книги: {content}. ");
+            Console.ResetColor();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Console.InputEncoding = System.Text.Encoding.Unicode;
+            Title title1 = new Title() { name = "The Lord of the Rings" };
+            Author author1 = new Author() { author = "John R.R. Tolkien" };
+            Content content1 = new Content() { content = "one of the most famous works of the fantasy genre" };
+            Book book = new Book() { title = title1, author = author1, content = content1 };
+            book.title.Show();
+            book.author.Show();
+            book.content.Show();
+
+            Console.ReadKey();
+
+        }
+    }
+}
